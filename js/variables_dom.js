d510807cc841a79bbb048e1117e9ad8c76c74d44
juan9507 <juandavidnaranjo75@gmail.com>
2022-06-14 21:40:24 -0500

    primera mejora refatorizacion del codigo

---
+++ js/variables_dom.js
@@ -0,0 +1,7 @@
+//Variables a utilizar para el manejo del dom
+export const d = document;
+export const $table = d.querySelector(".crud-table");
+export const $form = d.querySelector(".crud-form");
+export const $title = d.querySelector(".crud-title");
+export const $template = d.getElementById("crud-template").content;
+export const $fragment = d.createDocumentFragment();
